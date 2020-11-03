9440cf75d5cf95d293660f40655f6fd43fd921e5
Sidany Walker <sidanyk@gmail.com>
2020-11-03 14:36:08 -0500

    Initial Commit

---
+++ TipSplitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipSplitter
+//
+//  Created by Sidany Walker on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
